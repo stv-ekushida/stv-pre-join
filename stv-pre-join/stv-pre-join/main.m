6d35925f2956abb7d226a29b7fb0f3198ddd7735
TAKAHASHI Kazto <kazto@kazto.net>
2019-06-11 15:08:22 +0900

    課題1.1.2

--- stv-pre-join/stv-pre-join/main.m
+++ stv-pre-join/stv-pre-join/main.m
@@ -21,11 +21,32 @@ void exam_1_1_1(void) {
     NSLog(@"NSNumber: %d", [nValue intValue]);
 }
 
+void exam_1_1_2(void) {
+    NSArray *array = [[NSArray alloc] initWithObjects: @"foo", @"bar", @"buz", nil];
+    NSDictionary *dict = [
+        [NSDictionary alloc] initWithObjectsAndKeys:
+            @"val1", @"key1",
+            @"val2", @"key2",
+            nil];
+
+    for(int i = 0; i < [array count]; i++) {
+        NSString *v = [array objectAtIndex:i];
+        NSLog(@"array[%d] = %s", i, [v UTF8String]);
+    }
+
+    NSArray *keys = [dict allKeys];
+    for(int i = 0; i < [keys count]; i++) {
+        NSString *k = [keys objectAtIndex: i];
+        NSString *v = [dict objectForKey: k];
+        NSLog(@"dict[%s] = %s", [k UTF8String], [v UTF8String]);
+    }
+}
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
         exam_1_1_1();
-        
+        exam_1_1_2();
+
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
